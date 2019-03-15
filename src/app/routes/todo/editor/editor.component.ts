a788f13f533d3c2e866815469e68ab17c5318ec0
liwei <liwei@huayingjuhe.com>
2019-03-15 16:25:19 +0800

    复制图片上传显示

--- src/app/routes/todo/editor/editor.component.ts
+++ src/app/routes/todo/editor/editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, VERSION, OnInit } from '@angular/core';
 import { excelToMarkdown, looksLikeTable } from './excel-markdown-tables';
+import { copyImagePasteUpload } from './copy-image-paste-upload';
 
 @Component({
   selector: 'app-todo-editor',
@@ -39,10 +40,12 @@ s = "Python syntax highlighting"
 
   ngOnInit() {
     this.copyExcelPasteMarkdown();
+    this.documentPasteEvent();
   }
   copyExcelPasteMarkdown() {
     const editor = document.getElementById('editor');
     editor.addEventListener('paste', event => {
+      console.log(event);
       const clipboard = event.clipboardData;
       const data = clipboard.getData('text/plain').trim();
       if (looksLikeTable(data)) {
@@ -50,8 +53,27 @@ s = "Python syntax highlighting"
       } else {
         return;
       }
-      event.target['value'] += excelToMarkdown(data);;
+      event.target['value'] += excelToMarkdown(data);
       return false;
+    });
+  }
+  copyFilePasteUpload() {
+    const div = document.getElementById('adjunct');
+    div.addEventListener('paste', event => {
+      console.log(event);
+    });
+  }
+  documentPasteEvent() {
+    document.addEventListener('paste', (event) => {
+      console.log(event);
+      copyImagePasteUpload(event, '', function(responseText){
+        const data = JSON.parse(responseText),
+          tarBox = document.getElementById('tar_box');
+        const img = document.createElement('img');
+        img.className = 'my_img';
+        img.src = data.store_path;
+        tarBox.appendChild(img);
+      });
     });
   }
 }
